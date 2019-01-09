35f6e83ec901f9f2014c9d8628cc9054551183dc
isaacsutor <isaac.sutor@gmail.com>
2019-01-09 18:50:35 -0500

    data pre-processed and checked on first 25 images

--- tensorflow_intro.py
+++ tensorflow_intro.py
@@ -10,4 +10,28 @@ class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
 
+print(train_images.shape)
+print(len(train_labels))
+
+# show the image of an image
+plt.interactive(False)
+plt.figure()
+plt.imshow(train_images[0])
+plt.colorbar()
+plt.grid(False)
+plt.show(block=True)
+
+# preproccess data
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5, 5, i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[train_labels[i]])
+    plt.show(block = True)
 
