5fdb7821b8794c9bd941c3d63e01e807088af5f4
isaacsutor <isaac.sutor@gmail.com>
2019-01-11 20:00:43 -0500

    imports added

---
+++ tensorflow_regression.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import, division, print_function
+
+import pathlib
+
+import pandas as pd
+import seaborn as sns
+
+import tensorflow as tf
+import keras
+from keras import layers
+
