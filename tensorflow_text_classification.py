69551802a0edc5ee6baa2468e8ddc7c3eaec3b36
isaacsutor <isaac.sutor@gmail.com>
2019-01-11 19:53:34 -0500

    data loaded

--- tensorflow_text_classification.py
+++ tensorflow_text_classification.py
@@ -2,6 +2,6 @@ import tensorflow as tf
 import keras
 import numpy as np
 
-imbd = keras.datasets.imbd
+imdb = keras.datasets.imdb
 
-(train_data, train_labels), (test_data, test_labels) = imbd.load_data(num_words=10000)
+(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
